851e76998ada73b3223b68f94a5bbe97713de88f
LowFasT9 <74370261+LowFasT9@users.noreply.github.com>
2023-06-30 07:49:45 -0300

    Update_hinata.py
    latest version

--- hin_a/hinata.py
+++ hin_a/hinata.py
@@ -9,14 +9,18 @@ import webbrowser
 from googlesearch import search
 from PyQt5 import QtCore, QtGui, QtWidgets  # for creating a gui
 from PyQt5.QtGui import QMovie  # for playing animated gif on the window of the application
-import sys
+import sys, os
 
 class Ui_MainWindow(object):
     listener = sr.Recognizer()  # to recognise voice commands
-    engine = pyttsx3.init()     # initialize test to speech
+    engine = pyttsx3.init()  # initialize test to speech
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)   # setting voice 1 for female and 0 for male
+    engine.setProperty('voice', voices[1].id)
     def setupUi(self, MainWindow):
+        def resource_path(relative_path):
+            if hasattr(sys, '_MEIPASS'):
+                return os.path.join(sys._MEIPASS, relative_path)
+            return os.path.join(os.path.abspath("."), relative_path)
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(550, 550)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -40,16 +44,23 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
 
         # set movie as label
-        self.movie = QMovie("hinata.gif")   # path to the gif or if the gif is in same folder then name of the gif
+        self.movie = QMovie(resource_path('hinata.gif'))   # path to the gif or if the gif is in same folder then name of the gif
         self.label.setMovie(self.movie)
         self.movie.start()
 
-        self.pushbutton.clicked.connect(lambda: run_alexa())    # IMPORTANT : if you are connecting a method to a button then make sure to use lambda as follows or it will throw error
+        #self.pushbutton.clicked.connect(lambda: run_alexa())    # IMPORTANT : if you are connecting a method to a button then make sure to use lambda as follows or it will throw error
         # main function to run the voice assistant
-        def run_alexa():
-            command = take_command()
-            print(command)
-            if 'play' in command:   # if user says play
+        def take_command():
+            with sr.Microphone() as source:  # use the system microphone as source
+                self.listener.adjust_for_ambient_noise(source)
+                print("listening")
+                voice = self.listener.listen(source, 10)   # take input from the microphone
+                command = self.listener.recognize_google(voice)    # using google to understand the command
+                command = command.lower()       # converting the command in lowercase
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+            elif 'play' in command:   # if user says play
                 song = command.replace('play', '')
                 talk('playing' + song)
                 print(song)
@@ -67,7 +78,7 @@ class Ui_MainWindow(object):
                 talk(command)
                 command = command.replace('search', '')
                 query = command
-                for url in search(query, tld="co.in", num=1, stop=1, pause=1):  # search for the command and return the url link
+                for url in search(query,num_results=1):  # search for the command and return the url link
                     print(url)
                 webbrowser.open_new(str(url))   # open the web browser search for the retrieved url link
             else:
@@ -77,29 +88,16 @@ class Ui_MainWindow(object):
             self.engine.say(text)
             self.engine.runAndWait()
 
-        def take_command():
-            try:
-                with sr.Microphone() as source:  # use the system microphone as source
-                    self.listener.adjust_for_ambient_noise(source)
-                    print("listening")
-                    voice = self.listener.listen(source)   # take input from the microphone
-                    command = self.listener.recognize_google(voice)    # using google to understand the command
-                    command = command.lower()       # converting the command in lowercase
-                    if 'alexa' in command:
-                        command = command.replace('alexa', '')
-                        print(command)
-            except:
-                print("pass")
-                pass
-            return command
+        self.pushbutton.clicked.connect(lambda: take_command())  # IMPORTANT : if you are connecting a method to a button then make sure to use lambda as follows or it will throw error
+        # main function to run the voice assistant
         # gui code
+
+import sys
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(window)
     window.show()
-    try:
-        sys.exit(app.exec_())
-    except:
-        print("exit")
+    sys.exit(app.exec_())
